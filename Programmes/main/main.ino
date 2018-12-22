ea24bbeedff3c8ba5486463de489ed6eaa929560
SirHenryAllen <matt.lefranc@gmail.com>
2018-12-23 00:05:40 +0100

    mainProgramme

--- Programmes/main/main.ino
+++ Programmes/main/main.ino
@@ -1,5 +1,22 @@
+#define eGSalonB 7
+#define eDSalonB 8
+#define eGSM 9
+#define eDSM 10
+#define eGSalonEa 11
+#define eDSalonEa 12
+#define eGSalonEb 13
+#define eDSalonEb 14
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  pinMode(eGSalonB, HIGH);
+  pinMode(eDSalonB, HIGH);
+  pinMode(eGSM, HIGH);
+  pinMode(eDSM, HIGH);
+  pinMode(eGSalonEa, HIGH);
+  pinMode(eDSalonEa, HIGH);
+  pinMode(eGSalonEb, HIGH);
+  pinMode(eDSalonEb, HIGH);
 
 }
 
