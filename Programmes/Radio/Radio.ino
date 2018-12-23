2e3458b435a3f527de8b1c936b12540aee8dd77a
SirHenryAllen <matt.lefranc@gmail.com>
2018-12-23 22:54:51 +0100

    fail

--- Programmes/Radio/Radio.ino
+++ Programmes/Radio/Radio.ino
@@ -5,20 +5,51 @@
 
 TEA5767Radio radio = TEA5767Radio();
 
+#define pFreq A0
+
 float sData;
 float freq;
+float oldPFreq;
+float oldSData;
 
 void setup() { 
+
+  pinMode(pFreq, INPUT);
   
   Wire.begin();
   Serial.begin(9600);
   
 }
 
+
 void loop() {
 
-  sData = Serial.read();
-  freq = sData;
+  potar();
+  serial(); 
   radio.setFrequency(freq);
+  
+}
+
+
+void potar() {
+
+  if (((oldPFreq + 1) or (oldPFreq - 1) or (oldPFreq)) != analogRead(pFreq)) {
+     freq = ((analogRead(pFreq))/1023)*110;
+  }
+  
+  oldPFreq = analogRead(pFreq);
+
+}
+
+
+void serial() {
+
+  sData = Serial.read();
+  
+  if (oldSData != sData) {
+    freq = sData;
+  }
+  
+  oldSData = analogRead(sData);
   
 }
