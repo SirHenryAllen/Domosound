ac77ad894170c857fd30525886b2750cdf372c3c
SirHenryAllen <matt.lefranc@gmail.com>
2018-12-23 22:07:36 +0100

    mainProgramme

--- Programmes/main/main.ino
+++ Programmes/main/main.ino
@@ -1,16 +1,26 @@
-#define eGSalonB 7
-#define eDSalonB 8
+#define eGLRB 7
+#define eDLRB 8
 #define eGSM 9
 #define eDSM 10
-#define eGSalonEa 11
-#define eDSalonEa 12
-#define eGSalonEb 13
-#define eDSalonEb 14
+#define eGLREa 11
+#define eDLREa 12
+#define eGLREb 13
+#define eDLREb 14
 
-#define eSalonBS 15
+#define eLRBS 15
 #define eSMS 16
-#define eSalonEaS 17
-#define eSalonEbS 28
+#define eLREaS 17
+#define eLREbS 18
+
+#define ledUn 19
+#define ledDeux 20
+#define ledTrois 21
+#define ledQuatre 22
+
+#define dataInputUn 23
+#define dataInputDeux 24
+#define dataInputTrois 25
+#define dataInputQuatre 26
 
 int bUn = 0;
 int bDeux = 0;
@@ -21,20 +31,30 @@ int sData;
 
 void setup() {
   
-  pinMode(eGSalonB, HIGH);
-  pinMode(eDSalonB, HIGH);
-  pinMode(eGSM, HIGH);
-  pinMode(eDSM, HIGH);
-  pinMode(eGSalonEa, HIGH);
-  pinMode(eDSalonEa, HIGH);
-  pinMode(eGSalonEb, HIGH);
-  pinMode(eDSalonEb, HIGH);
+  pinMode(eGLRB, OUTPUT);
+  pinMode(eDLRB, OUTPUT);
+  pinMode(eGSM, OUTPUT);
+  pinMode(eDSM, OUTPUT);
+  pinMode(eGLREa, OUTPUT);
+  pinMode(eDLREa, OUTPUT);
+  pinMode(eGLREb, OUTPUT);
+  pinMode(eDLREb, OUTPUT);
 
-  pinMode(eSalonBS, INPUT_PULLUP);
+  pinMode(eLRBS, INPUT_PULLUP);
   pinMode(eSMS, INPUT_PULLUP);
-  pinMode(eSalonEaS, INPUT_PULLUP);
-  pinMode(eSalonEbS, INPUT_PULLUP);
+  pinMode(eLREaS, INPUT_PULLUP);
+  pinMode(eLREbS, INPUT_PULLUP);
 
+  pinMode(ledUn, OUTPUT);
+  pinMode(ledDeux, OUTPUT);
+  pinMode(ledTrois, OUTPUT);
+  pinMode(ledQuatre, OUTPUT);
+
+  pinMode(dataInputUn, OUTPUT);
+  pinMode(dataInputDeux, OUTPUT);
+  pinMode(dataInputTrois, OUTPUT);
+  pinMode(dataInputQuatre, OUTPUT);
+  
   Serial.begin(9600);
   
 }
@@ -43,7 +63,7 @@ void loop() {
 
   sData = Serial.read();
   
-  if (((digitalRead(eSalonBS)) == 0) or (sData == 1)) {
+  if (((digitalRead(eLRBS)) == 0) or (sData == 1)) {
     bUn = bUn + 1;
     if (bUn == 2) {
       bUn = 0;
@@ -55,17 +75,59 @@ void loop() {
       bDeux = 0;
     }
   }
-  if (((digitalRead(eSalonEaS)) == 0) or (sData == 3)) {
+  if (((digitalRead(eLREaS)) == 0) or (sData == 3)) {
     bTrois = bTrois + 1;
     if (bTrois == 2) {
       bTrois = 0;
     }
   }
-  if (((digitalRead(eSalonEbS)) == 0) or (sData == 4)) {
+  if (((digitalRead(eLREbS)) == 0) or (sData == 4)) {
     bQuatre = bQuatre + 1;
     if (bQuatre == 2) {
       bQuatre = 0;
     }
   }
 
+  if (bUn == 1) {
+    digitalWrite(eGLRB, HIGH);
+    digitalWrite(eDLRB, HIGH);
+    digitalWrite(ledUn, HIGH);
+  }
+  if (bDeux == 1) {
+    digitalWrite(eGSM, HIGH);
+    digitalWrite(eDSM, HIGH);
+    digitalWrite(ledDeux, HIGH);
+  }
+  if (bTrois == 1) {
+    digitalWrite(eGLREa, HIGH);
+    digitalWrite(eDLREa, HIGH);
+    digitalWrite(ledTrois, HIGH);
+  }
+  if (bQuatre == 1) {
+    digitalWrite(eGLREb, HIGH);
+    digitalWrite(eDLREb, HIGH);
+    digitalWrite(ledQuatre, HIGH);
+  }
+
+  if (bUn == 0) {
+    digitalWrite(eGLRB, LOW);
+    digitalWrite(eDLRB, LOW);
+    digitalWrite(ledUn, LOW);
+  }
+  if (bDeux == 0) {
+    digitalWrite(eGSM, LOW);
+    digitalWrite(eDSM, LOW);
+    digitalWrite(ledDeux, LOW);
+  }
+  if (bTrois == 0) {
+    digitalWrite(eGLREa, LOW);
+    digitalWrite(eDLREa, LOW);
+    digitalWrite(ledTrois, LOW);
+  }
+  if (bQuatre == 0) {
+    digitalWrite(eGLREb, LOW);
+    digitalWrite(eDLREb, LOW);
+    digitalWrite(ledQuatre, LOW);
+  }
+  
 }
