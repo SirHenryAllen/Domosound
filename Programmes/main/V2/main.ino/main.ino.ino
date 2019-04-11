843f64964cee02c04593822d6afc6ab17b615c2d
Sir Henry Allen <matt.lefranc@gmail.com>
2019-04-11 23:09:48 +0200

    renommé :         Programmes/main/main.ino -> Programmes/main/V1/main/main.ino 	nouveau fichier : Programmes/main/V2/main.ino/main.ino.ino

---
+++ Programmes/main/V2/main.ino/main.ino.ino
@@ -0,0 +1,23 @@
+boolean tab[8];
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void control() {
+    
+}
+
+void tabBool (boolean b, int x) {
+  tab[x] = b;
+}
+
+void afficheTab() {
+  Serial.println(tab);
+}
